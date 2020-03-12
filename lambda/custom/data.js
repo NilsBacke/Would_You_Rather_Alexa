8149bbfef0b9fa189c052ec465dadf8bdf676423
Nils Backe <nils.backe@gmail.com>
2020-03-11 21:12:03 -0700

    initial commit

---
+++ lambda/custom/data.js
@@ -0,0 +1,353 @@
+module.exports = {
+	questions: [
+		{
+			choiceA: 'lead a boring life from here forward',
+			choiceB: 'be reborn with all your memories into a baby of the opposite sex',
+			choiceAPercentage: 23,
+			choiceBPercentage: 77
+		},
+		{
+			choiceA: 'listen to only one song for the rest of your life',
+			choiceB: 'listen to no music for the rest of your life',
+			choiceAPercentage: 72,
+			choiceBPercentage: 28
+		},
+		{
+			choiceA: 'have a month of intense nonstop rain',
+			choiceB: 'have a month long 100+ degree heatwave',
+			choiceAPercentage: 79,
+			choiceBPercentage: 21
+		},
+		{
+			choiceA: 'live in the past',
+			choiceB: 'live in the future',
+			choiceAPercentage: 29,
+			choiceBPercentage: 71
+		},
+		{
+			choiceA: 'have the voice of PeeWee Herman and dress perfectly',
+			choiceB: 'have the voice of Morgan Freeman and dress terribly',
+			choiceAPercentage: 25,
+			choiceBPercentage: 75
+		},
+		{
+			choiceA: 'never be able to leave your native country',
+			choiceB: 'never be able to drive',
+			choiceAPercentage: 58,
+			choiceBPercentage: 42
+		},
+		{
+			choiceA: 'be a unicorn',
+			choiceB: 'be a wizard',
+			choiceAPercentage: 24,
+			choiceBPercentage: 76
+		},
+		{
+			choiceA: 'not see what is far away',
+			choiceB: 'not see what is close up',
+			choiceAPercentage: 70,
+			choiceBPercentage: 30
+		},
+		{
+			choiceA: 'superhuman memory ability to retain all information you learn',
+			choiceB: 'be able to teleport',
+			choiceAPercentage: 32,
+			choiceBPercentage: 68
+		},
+		{
+			choiceA: 'have a hot spouse but be poor',
+			choiceB: 'be homosexual with a famous actor and be rich',
+			choiceAPercentage: 58,
+			choiceBPercentage: 42
+		},
+		{
+			choiceA: 'eat nothing for two days',
+			choiceB: 'eat the same meal for a month',
+			choiceAPercentage: 42,
+			choiceBPercentage: 58
+		},
+		{
+			choiceA: 'always drive under the speeding limit',
+			choiceB: 'always drive over the speeding limit',
+			choiceAPercentage: 56,
+			choiceBPercentage: 44
+		},
+		{
+			choiceA: 'get severe frostbite',
+			choiceB: 'get severely burned',
+			choiceAPercentage: 64,
+			choiceBPercentage: 36
+		},
+		{
+			choiceA: 'meet Chuck Norris',
+			choiceB: 'meet the Old Spice man',
+			choiceAPercentage: 63,
+			choiceBPercentage: 37
+		},
+		{
+			choiceA: 'be a robot',
+			choiceB: 'be an alien',
+			choiceAPercentage: 47,
+			choiceBPercentage: 53
+		},
+		{
+			choiceA: 'be looked at awkwardly',
+			choiceB: 'be looked at with disgust',
+			choiceAPercentage: 84,
+			choiceBPercentage: 16
+		},
+		{
+			choiceA: 'always be hot',
+			choiceB: 'always be cold',
+			choiceAPercentage: 43,
+			choiceBPercentage: 57
+		},
+		{
+			choiceA: 'not be able to smell',
+			choiceB: 'not be able to taste',
+			choiceAPercentage: 76,
+			choiceBPercentage: 24
+		},
+		{
+			choiceA: 'be bald everywhere',
+			choiceB: 'be albino',
+			choiceAPercentage: 58,
+			choiceBPercentage: 42
+		},
+		{
+			choiceA: 'be a hometown hero',
+			choiceB: 'be a C level celebrity no one really likes',
+			choiceAPercentage: 89,
+			choiceBPercentage: 11
+		},
+		{
+			choiceA: 'win a 1-day shopping spree to any store',
+			choiceB: 'win a 2-week vacation to any destination',
+			choiceAPercentage: 42,
+			choiceBPercentage: 58
+		},
+		{
+			choiceA: 'be a shark',
+			choiceB: 'be a dolphin',
+			choiceAPercentage: 46,
+			choiceBPercentage: 54
+		},
+		{
+			choiceA: 'be a horse jockey',
+			choiceB: 'be a NASCAR driver',
+			choiceAPercentage: 27,
+			choiceBPercentage: 73
+		},
+		{
+			choiceA: 'end world hunger',
+			choiceB: 'find a cure for cancer',
+			choiceAPercentage: 36,
+			choiceBPercentage: 64
+		},
+		{
+			choiceA: 'have grown up without a father',
+			choiceB: 'have grown up without a mother',
+			choiceAPercentage: 74,
+			choiceBPercentage: 26
+		},
+		{
+			choiceA: 'drive a smart car',
+			choiceB: 'drive a Lamborghini and fill up 4 times a week',
+			choiceAPercentage: 45,
+			choiceBPercentage: 55
+		},
+		{
+			choiceA: 'know everything and die tomorrow',
+			choiceB: 'live forever and be ignorant',
+			choiceAPercentage: 44,
+			choiceBPercentage: 56
+		},
+		{
+			choiceA: 'death by lethal injection',
+			choiceB: 'death by electric chair',
+			choiceAPercentage: 75,
+			choiceBPercentage: 25
+		},
+		{
+			choiceA: 'be credited with the invention of the wheel',
+			choiceB: 'be credited with the invention of the Internet',
+			choiceAPercentage: 14,
+			choiceBPercentage: 86
+		},
+		{
+			choiceA: 'have extremely muscular arms',
+			choiceB: 'have extremely muscular legs',
+			choiceAPercentage: 69,
+			choiceBPercentage: 31
+		},
+		{
+			choiceA: 'kill one person you know',
+			choiceB: 'kill three strangers',
+			choiceAPercentage: 36,
+			choiceBPercentage: 64
+		},
+		{
+			choiceA: 'be a very fat and ugly person with a ton of friends that will never get someone of the opposite sex',
+			choiceB:
+				'be a very skinny and attractive person that has no friends but can get any person of the opposite sex they want',
+			choiceAPercentage: 22,
+			choiceBPercentage: 78
+		},
+		{
+			choiceA: 'be convicted as a corrupt politician',
+			choiceB: 'be convicted as a corrupt Fortune 500 CEO',
+			choiceAPercentage: 36,
+			choiceBPercentage: 64
+		},
+		{
+			choiceA: 'see how or what created civilization',
+			choiceB: 'see how or what ends civilization',
+			choiceAPercentage: 46,
+			choiceBPercentage: 54
+		},
+		{
+			choiceA: 'have an incredibly nice car',
+			choiceB: 'own a personal helicopter',
+			choiceAPercentage: 52,
+			choiceBPercentage: 48
+		},
+		{
+			choiceA: 'reach your ideal salary',
+			choiceB: 'reach your ideal weight',
+			choiceAPercentage: 69,
+			choiceBPercentage: 31
+		},
+		{
+			choiceA: 'go to a different planet',
+			choiceB: 'travel through time',
+			choiceAPercentage: 15,
+			choiceBPercentage: 85
+		},
+		{
+			choiceA: 'be loved',
+			choiceB: 'be feared',
+			choiceAPercentage: 82,
+			choiceBPercentage: 18
+		},
+		{
+			choiceA: 'have the sense of smell of a dog',
+			choiceB: 'have the eyesight of an eagle',
+			choiceAPercentage: 13,
+			choiceBPercentage: 87
+		},
+		{
+			choiceA: 'have a British accent',
+			choiceB: 'have an Australian accent',
+			choiceAPercentage: 62,
+			choiceBPercentage: 38
+		},
+		{
+			choiceA: 'master every programming language',
+			choiceB: 'master every spoken language',
+			choiceAPercentage: 23,
+			choiceBPercentage: 7
+		},
+		{
+			choiceA: 'get 4 hours of sleep per night',
+			choiceB: 'get 15 hours of sleep per night',
+			choiceAPercentage: 41,
+			choiceBPercentage: 59
+		},
+		{
+			choiceA: 'meet your future self',
+			choiceB: 'meet your future kids',
+			choiceAPercentage: 62,
+			choiceBPercentage: 38
+		},
+		{
+			choiceA: 'have the ability to teleport',
+			choiceB: 'have the ability to time travel',
+			choiceAPercentage: 50,
+			choiceBPercentage: 50
+		},
+		{
+			choiceA: 'be in jail for a year',
+			choiceB: 'live in complete isolation in the mountains for a year',
+			choiceAPercentage: 32,
+			choiceBPercentage: 68
+		},
+		{
+			choiceA: 'be allergic to peanut butter',
+			choiceB: 'be allergic to chocolate',
+			choiceAPercentage: 82,
+			choiceBPercentage: 18
+		},
+		{
+			choiceA: 'skip summer',
+			choiceB: 'skip winter',
+			choiceAPercentage: 32,
+			choiceBPercentage: 68
+		},
+		{
+			choiceA: 'eat a tablespoon of salt',
+			choiceB: 'eat a tablespoon of cinnamon',
+			choiceAPercentage: 54,
+			choiceBPercentage: 46
+		},
+		{
+			choiceA: 'be Zeus',
+			choiceB: 'be Poseidon',
+			choiceAPercentage: 69,
+			choiceBPercentage: 31
+		},
+		{
+			choiceA: 'be tall and really skinny',
+			choiceB: 'be buff and really short',
+			choiceAPercentage: 68,
+			choiceBPercentage: 32
+		},
+		{
+			choiceA: 'have a high salary in a job you hate',
+			choiceB: 'have a low salary in a job you love',
+			choiceAPercentage: 56,
+			choiceBPercentage: 44
+		},
+		{
+			choiceA: 'be best friends with Harry Potter',
+			choiceB: 'be best friends with Katniss Everdeen',
+			choiceAPercentage: 61,
+			choiceBPercentage: 39
+		},
+		{
+			choiceA: 'live in a tree house',
+			choiceB: 'live in a floating boat house',
+			choiceAPercentage: 59,
+			choiceBPercentage: 41
+		},
+		{
+			choiceA: 'be lost in the woods at night',
+			choiceB: 'be stuck in a haunted house at night',
+			choiceAPercentage: 71,
+			choiceBPercentage: 29
+		},
+		{
+			choiceA: 'eat fruit',
+			choiceB: 'eat vegetables',
+			choiceAPercentage: 88,
+			choiceBPercentage: 12
+		},
+		{
+			choiceA: 'control the elements',
+			choiceB: 'control time',
+			choiceAPercentage: 35,
+			choiceBPercentage: 65
+		},
+		{
+			choiceA: 'eat Skittles',
+			choiceB: 'eat M&Ms',
+			choiceAPercentage: 52,
+			choiceBPercentage: 48
+		},
+		{
+			choiceA: 'lick the floor at a club',
+			choiceB: 'lick a toilet seat at walmart',
+			choiceAPercentage: 88,
+			choiceBPercentage: 12
+		}
+	]
+}
--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,173 +1,207 @@
-/* *
- * This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
- * Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
- * session persistence, api calls, and more.
- * */
-const Alexa = require('ask-sdk-core');
-// i18n library dependency, we use it below in a localisation interceptor
-const i18n = require('i18next');
-// i18n strings for all supported locales
-const languageStrings = require('./languageStrings');
+const Alexa = require('ask-sdk-core')
+const QUESTIONS = require('./data.js').questions
+
+var currentQuestion = undefined
 
 const LaunchRequestHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
-    },
-    handle(handlerInput) {
-        const speakOutput = handlerInput.t('WELCOME_MSG');
+	canHandle(handlerInput) {
+		return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest'
+	},
+	handle(handlerInput) {
+		return askQuestion(handlerInput, true)
+	}
+}
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
-    }
-};
+const WouldYouRatherHandler = {
+	canHandle(handlerInput) {
+		return (
+			Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+			Alexa.getIntentName(handlerInput.requestEnvelope) === 'WouldYouRather'
+		)
+	},
+	handle(handlerInput) {
+		return askQuestion(handlerInput)
+	}
+}
 
-const HelloWorldIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
-    },
-    handle(handlerInput) {
-        const speakOutput = handlerInput.t('HELLO_MSG');
+function askQuestion(handlerInput, welcome) {
+	if (welcome === undefined) {
+		welcome = false
+	}
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
-    }
-};
+	currentQuestion = QUESTIONS[Math.floor(Math.random() * QUESTIONS.length)]
+
+	var speakOutput = 'Would you rather, a:, ' + currentQuestion.choiceA + ', or, b:, ' + currentQuestion.choiceB + '?'
+
+	if (welcome) {
+		speakOutput =
+			"Welcome to Would you rather. Say 'a' or 'b' to select your answer. Here's your question: " + speakOutput
+	}
+
+	return handlerInput.responseBuilder
+		.speak(speakOutput)
+		.reprompt(speakOutput)
+		.getResponse()
+}
+
+const ChoiceAIntentHandler = {
+	canHandle(handlerInput) {
+		return (
+			Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+			Alexa.getIntentName(handlerInput.requestEnvelope) === 'ChoiceAIntent'
+		)
+	},
+	handle(handlerInput) {
+		const speakOutput =
+			currentQuestion.choiceAPercentage + " percent of people agree with you. Say 'again' for another question."
+		return handlerInput.responseBuilder
+			.speak(speakOutput)
+			.reprompt(speakOutput)
+			.getResponse()
+	}
+}
+
+const ChoiceBIntentHandler = {
+	canHandle(handlerInput) {
+		return (
+			Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+			Alexa.getIntentName(handlerInput.requestEnvelope) === 'ChoiceBIntent'
+		)
+	},
+	handle(handlerInput) {
+		const speakOutput =
+			currentQuestion.choiceBPercentage + " percent of people agree with you. Say 'again' for another question."
+		return handlerInput.responseBuilder
+			.speak(speakOutput)
+			.reprompt(speakOutput)
+			.getResponse()
+	}
+}
 
 const HelpIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
-    },
-    handle(handlerInput) {
-        const speakOutput = handlerInput.t('HELP_MSG');
+	canHandle(handlerInput) {
+		return (
+			Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+			Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent'
+		)
+	},
+	handle(handlerInput) {
+		const speakOutput = "Say 'play' to start playing. Say 'cancel' to exit."
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
-    }
-};
+		return handlerInput.responseBuilder
+			.speak(speakOutput)
+			.reprompt(speakOutput)
+			.getResponse()
+	}
+}
 
 const CancelAndStopIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
-                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
-    },
-    handle(handlerInput) {
-        const speakOutput = handlerInput.t('GOODBYE_MSG');
+	canHandle(handlerInput) {
+		return (
+			Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+			(Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent' ||
+				Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent')
+		)
+	},
+	handle(handlerInput) {
+		const speakOutput = "Goodbye! Say 'Alexa, play would you rather' next time you want to play."
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .getResponse();
-    }
-};
+		return handlerInput.responseBuilder.speak(speakOutput).getResponse()
+	}
+}
 /* *
  * FallbackIntent triggers when a customer says something that doesn’t map to any intents in your skill
  * It must also be defined in the language model (if the locale supports it)
- * This handler can be safely added but will be ingnored in locales that do not support it yet 
+ * This handler can be safely added but will be ingnored in locales that do not support it yet
  * */
 const FallbackIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
-    },
-    handle(handlerInput) {
-        const speakOutput = handlerInput.t('FALLBACK_MSG');
+	canHandle(handlerInput) {
+		return (
+			Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+			Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent'
+		)
+	},
+	handle(handlerInput) {
+		const speakOutput = "I'm sorry, I didn't understand that. Say 'play' to begin playing."
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
-    }
-};
+		return handlerInput.responseBuilder
+			.speak(speakOutput)
+			.reprompt(speakOutput)
+			.getResponse()
+	}
+}
 /* *
- * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
- * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
- * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
+ * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open
+ * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not
+ * respond or says something that does not match an intent defined in your voice model. 3) An error occurs
  * */
 const SessionEndedRequestHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
-    },
-    handle(handlerInput) {
-        console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
-        // Any cleanup logic goes here.
-        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
-    }
-};
+	canHandle(handlerInput) {
+		return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest'
+	},
+	handle(handlerInput) {
+		console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`)
+		// Any cleanup logic goes here.
+		return handlerInput.responseBuilder.getResponse() // notice we send an empty response
+	}
+}
 /* *
  * The intent reflector is used for interaction model testing and debugging.
- * It will simply repeat the intent the user said. You can create custom handlers for your intents 
- * by defining them above, then also adding them to the request handler chain below 
+ * It will simply repeat the intent the user said. You can create custom handlers for your intents
+ * by defining them above, then also adding them to the request handler chain below
  * */
 const IntentReflectorHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
-    },
-    handle(handlerInput) {
-        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
-        const speakOutput = handlerInput.t('REFLECTOR_MSG', {intentName: intentName});
+	canHandle(handlerInput) {
+		return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+	},
+	handle(handlerInput) {
+		const intentName = Alexa.getIntentName(handlerInput.requestEnvelope)
+		const speakOutput = 'You called ' + intentName
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
-    }
-};
+		return (
+			handlerInput.responseBuilder
+				.speak(speakOutput)
+				//.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+				.getResponse()
+		)
+	}
+}
 /**
  * Generic error handling to capture any syntax or routing errors. If you receive an error
  * stating the request handler chain is not found, you have not implemented a handler for
- * the intent being invoked or included it in the skill builder below 
+ * the intent being invoked or included it in the skill builder below
  * */
 const ErrorHandler = {
-    canHandle() {
-        return true;
-    },
-    handle(handlerInput, error) {
-        const speakOutput = handlerInput.t('ERROR_MSG');
-        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+	canHandle() {
+		return true
+	},
+	handle(handlerInput, error) {
+		const speakOutput = "I'm sorry, there was an error. Please try again later."
+		console.log(`~~~~ Error handled: ${JSON.stringify(error)}`)
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
-    }
-};
+		return handlerInput.responseBuilder
+			.speak(speakOutput)
+			.reprompt(speakOutput)
+			.getResponse()
+	}
+}
 
-// This request interceptor will bind a translation function 't' to the handlerInput
-const LocalisationRequestInterceptor = {
-    process(handlerInput) {
-        i18n.init({
-            lng: Alexa.getLocale(handlerInput.requestEnvelope),
-            resources: languageStrings
-        }).then((t) => {
-            handlerInput.t = (...args) => t(...args);
-        });
-    }
-};
 /**
  * This handler acts as the entry point for your skill, routing all request and response
  * payloads to the handlers above. Make sure any new handlers or interceptors you've
- * defined are included below. The order matters - they're processed top to bottom 
+ * defined are included below. The order matters - they're processed top to bottom
  * */
 exports.handler = Alexa.SkillBuilders.custom()
-    .addRequestHandlers(
-        LaunchRequestHandler,
-        HelloWorldIntentHandler,
-        HelpIntentHandler,
-        CancelAndStopIntentHandler,
-        FallbackIntentHandler,
-        SessionEndedRequestHandler,
-        IntentReflectorHandler)
-    .addErrorHandlers(
-        ErrorHandler)
-    .addRequestInterceptors(
-        LocalisationRequestInterceptor)
-    .withCustomUserAgent('sample/hello-world/v1.2')
-    .lambda();
+	.addRequestHandlers(
+		LaunchRequestHandler,
+		WouldYouRatherHandler,
+		ChoiceAIntentHandler,
+		ChoiceBIntentHandler,
+		HelpIntentHandler,
+		CancelAndStopIntentHandler,
+		FallbackIntentHandler,
+		SessionEndedRequestHandler,
+		IntentReflectorHandler
+	)
+	.addErrorHandlers(ErrorHandler)
+	.lambda()
